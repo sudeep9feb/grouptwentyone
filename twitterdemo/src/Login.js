fdb2be36753e44ba52fc7b683557df2cbb3bcf86
Sudeep <16595513+sudeep9feb@users.noreply.github.com>
2023-03-02 22:57:30 +0530

    Fixed Login issues

--- twitterdemo/src/Login.js
+++ twitterdemo/src/Login.js
@@ -1,51 +1,53 @@
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import { useFormik } from 'formik';
 import React from 'react'
-import * as yup from 'yup';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import * as Yup from "yup"
+import { useFormik } from 'formik';
 
 function Login() {
-
-    // validation rules with Yup
-    const formSchema = yup.object({
-        email: yup.string().email("Not a valid email").required("Email is required"),
-        password: yup.string().required("Password is required"),
-        username: yup.string().required("Username is required").
-            min(5, "no less than 5 schracters").
-            max(10, "no more than 10 characters")
+    const loginSchema = Yup.object().shape({
+        username: Yup.string().required("Username is required").min(5, "no less than 5 characters"),
+        email: Yup.string().email("Enter a valid email address").required("Email is required"),
+        password: Yup.string().required("Password is required").max(10, "no more than 10 characters")
+            .matches(/[@$!%*#?&]+/, "One special character")
     })
 
-    // form handling with formik
     const formik = useFormik({
         initialValues: {
             username: "",
             email: "",
             password: ""
         },
-        validateSchema: formSchema,
+        validationSchema: loginSchema,
         onSubmit: (values) => {
-            console.log(values);
+            console.log("Submitting");
+            setTimeout(() => {
+                console.log(values.username);
+                console.log(values.password);
+            }, 2000)
         }
     })
     return (
         <Box
-            display={"flex"}
-            flexDirection="column"
+            display="flex"
+            flexDirection={"column"}
             justifyContent="center"
-            margin={"auto"}
-            marginTop={20}
-            maxWidth="300px"
-            boxShadow={"3px 3px 3px 3px #ccc"}
-            padding={"20px"}
+            maxWidth={"350px"}
+            margin="auto"
+            marginTop={10}
+            boxShadow="3px 3px 3px 3px #ccc"
+            sx={{ "&:hover": { boxShadow: "4px 4px 4px 4px #ccc" } }}
+            padding={5}
+
         >
             <form onSubmit={formik.handleSubmit}>
-                <Stack direction={"column"} spacing={2}>
-                    <Typography variant="h3" sx={{ color: "#ccc" }}>Login Page</Typography>
+                <Stack direction={"column"} spacing={3}>
+                    <Typography sx={{ textAlign: "center", color: "#bbb" }} variant='h3'>Login Page</Typography>
                     <TextField
                         variant='filled'
                         label="Username"
-                        type={"text"}
-                        placeholder="Enter the username"
                         name="username"
+                        placeholder='PLease enter the username'
+                        type={"text"}
                         onChange={formik.handleChange}
                         error={Boolean(formik.touched.username && formik.errors.username)}
                         helperText={formik.touched.username && formik.errors.username}
@@ -53,28 +55,28 @@ function Login() {
                     <TextField
                         variant='filled'
                         label="Email"
+                        placeholder='PLease enter the email'
                         type={"email"}
                         name="email"
-                        placeholder="Enter the email"
                         onChange={formik.handleChange}
                         error={Boolean(formik.touched.email && formik.errors.email)}
-                        helperText={formik.touched.email && formik.errors.email}
-                    />
+                        helperText={formik.touched.email && formik.errors.email} />
                     <TextField
                         variant='filled'
                         label="Password"
+                        placeholder='PLease enter the password'
                         type={"password"}
                         name="password"
-                        placeholder="Enter the password"
                         onChange={formik.handleChange}
                         error={Boolean(formik.touched.password && formik.errors.password)}
                         helperText={formik.touched.password && formik.errors.password}
                     />
-                    <Button type="submit" variant="contained">Login</Button>
+                    <Button type="submit" sx={{ padding: "10px", borderRadius: "40px" }} variant="contained" >
+                        <Typography variant='h5'>Login</Typography>
+                    </Button>
                 </Stack>
             </form>
-        </Box >
-    )
+        </Box >)
 }
 
 export default Login
